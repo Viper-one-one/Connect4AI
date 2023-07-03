50e801ee062e945c15c56d02e9a795f5775bbeca
Jake Anderson <realjakeanderson@gmail.com>
2023-07-03 14:18:09 -0700

    added state space search ai

--- Connect4AI.py
+++ Connect4AI.py
@@ -40,7 +40,7 @@ AI_PIECE = 2
 RANDOM_AI = 1
 BLOCKING_AI = 2
 STATE_SPACE_SEARCH = 3
-
+WINDOW_LENGTH = 4
 #game functions and ai control
 def create_board():
     board = np.zeros((ROW_COUNT, COLUMN_COUNT))
@@ -108,6 +108,9 @@ def random_player():
     return 0
 
 def blocking_player():
+    return 0
+
+def state_space_player():
     return 0
 
 #pygame creation
@@ -238,6 +241,153 @@ def checkThreeInARow(board, piece):
 
 
 
+def evaluate_window(window, piece):
+	score = 0
+	opp_piece = PLAYER_PIECE
+	if piece == PLAYER_PIECE:
+		opp_piece = AI_PIECE
+
+	if window.count(piece) == 4:
+		score += 100
+	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+		score += 5
+	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+		score += 2
+
+	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+		score -= 4
+
+	return score
+
+
+def score_position(board, piece):
+	score = 0
+
+	## Score center column
+	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 3
+
+	## Score Horizontal
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(COLUMN_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score Vertical
+	for c in range(COLUMN_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
+
+	## Score posiive sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	return score
+
+
+def get_valid_locations(board):
+	valid_locations = []
+	for col in range(COLUMN_COUNT):
+		if is_valid(board, col):
+			valid_locations.append(col)
+	return valid_locations
+
+def is_terminal_node(board):
+	return check_win(board, PLAYER_PIECE) or check_win(board, AI_PIECE) or len(get_valid_locations(board)) == 0
+
+def minimax(board, depth, alpha, beta, maximizingPlayer):
+	valid_locations = get_valid_locations(board)
+	is_terminal = is_terminal_node(board)
+	if depth == 0 or is_terminal:
+		if is_terminal:
+			if check_win(board, AI_PIECE):
+				return (None, 100000000000000)
+			elif check_win(board, PLAYER_PIECE):
+				return (None, -10000000000000)
+			else: # Game is over, no more valid moves
+				return (None, 0)
+		else: # Depth is zero
+			return (None, score_position(board, AI_PIECE))
+	if maximizingPlayer:
+		value = -math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, AI_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+			if new_score > value:
+				value = new_score
+				column = col
+			alpha = max(alpha, value)
+			if alpha >= beta:
+				break
+		return column, value
+
+	else: # Minimizing player
+		value = math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, PLAYER_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+			if new_score < value:
+				value = new_score
+				column = col
+			beta = min(beta, value)
+			if alpha >= beta:
+				break
+		return column, value
+
+def pick_best_move(board, piece):
+
+	valid_locations = get_valid_locations(board)
+	best_score = -10000
+	best_col = random.choice(valid_locations)
+	for col in valid_locations:
+		row = get_next_open_row(board, col)
+		temp_board = board.copy()
+		drop_piece(temp_board, row, col, piece)
+		score = score_position(temp_board, piece)
+		if score > best_score:
+			best_score = score
+			best_col = col
+
+	return best_col
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 def play():
     game_over = False
     game_turns = 0
@@ -334,7 +484,24 @@ def play():
                 game_over = True
                 print_board(board)
                 draw_board(board)
-                
+            elif ai_type == STATE_SPACE_SEARCH:
+                #col = random.randint(0, COLUMN_COUNT-1)
+		        #col = pick_best_move(board, AI_PIECE)
+                col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
+                if is_valid(board, col):
+			        #pygame.time.wait(500)
+                    row = get_next_open_row(board, col)
+                    drop_piece(board, row, col, AI_PIECE)
+                    if check_win(board, AI_PIECE):
+                        label = my_font.render("Player 2 wins!!", 1, YELLOW)
+                        screen.blit(label, (40,10))
+                        game_over = True
+                        
+                    print_board(board)
+                    draw_board(board)
+                    
+                    turn += 1
+                    turn = turn % 2
 
         if game_over:
             pygame.time.wait(5000)
